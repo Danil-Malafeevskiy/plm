329119bfe144101a1a0475fec2a300b813436152
Данил <dmalafeevskii@bk.ru>
2022-06-23 13:21:35 +0300

    postgres_settings

--- plm/plm/settings.py
+++ plm/plm/settings.py
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'plm.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'den1042',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
