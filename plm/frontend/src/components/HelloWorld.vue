0148a5993c387ea1ee0cbfc8301105ae256cfe07
Maksim Khirug <maxkhirug@gmail.com>
2022-06-24 16:01:05 +0300

    Добавлена UI-библиотека и заглушка для Home страницы

---
+++ plm/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container>
+    <div id="container">
+      <div id="title">
+        Карта ЛЭП
+      </div>
+      <br>
+      <div id="subtitle">
+        Автоматизированное приложение для решения поставленных задач
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+  }
+</script>
+
+<style>
+
+  #title{
+    font-size: 96px;
+  }
+
+  #subtitle{
+    font-size: 32px;
+    color: #EF5350;
+  }
+
+  #container{
+    margin-top: 8em;
+    margin-left: 1em;
+    opacity: 0;
+    animation: ani 1.5s forwards;
+    
+  }
+
+  @keyframes ani {
+  0% {opacity: 0;}
+  100% {opacity: 1;}
+
+}
+
+
+</style>
