11dd863d4c29baac5e8cb71c6c66293bbe7695ed
Maksim Khirug <maxkhirug@gmail.com>
2022-06-24 13:47:01 +0300

    Добавлена заглушка домашней страницы, создана директория components/CommonPages для страниц

--- plm/frontend/src/App.vue
+++ plm/frontend/src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    
+    <img alt="KT logo" 
+      src="https://upload.wikimedia.org/wikipedia/commons/7/7c/AO_Kronstadt_Group_logo.jpg"
+      width="297.3"
+      height="91.7"
+    >
+    <HomePage msg="КРОНШТАДТ Х IT GANG"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/CommonPages/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HomePage
+}
 }
 </script>
 
--- plm/frontend/vue.config.js
+++ plm/frontend/vue.config.js
@@ -1,14 +1,4 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/static/dist/' : 'http://localhost:8080',
-    outputDir: '../static/dist',
-    indexPath: '../../templates/base-vue.html',
-
-    devServer:{
-        devMiddleware:{
-          writeToDisk : filePath => filePath.endsWith("index.html"),
-          headers: {"Acess-Control-Allow-Origin":"*"},
-          publicPath: "http://localhost:8080",
-        },
-        hot: 'only',
-    }
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
