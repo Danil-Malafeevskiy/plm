603bf15b4a3bd3939cf917c80e5d031d394dbd08
Maksim Khirug <maxkhirug@gmail.com>
2022-06-30 11:42:34 +0300

    Исправлена ошибка карты, которая не позволяла изменять ее

--- plm/frontend/src/components/Map/MapArea.vue
+++ plm/frontend/src/components/Map/MapArea.vue
@@ -1,25 +1,40 @@
 <template>
   <v-content id="content">
-    <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true" style="height: 600px; width: 1200px;">
-        <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
+    <vl-map data-projection="EPSG:4326" style="height: 37.5em; width: 75em;">
+      <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
 
-        <vl-layer-tile id="osm">
-            <vl-source-osm></vl-source-osm>
-        </vl-layer-tile>
+      <vl-layer-tile>
+        <vl-source-osm></vl-source-osm>
+      </vl-layer-tile>
+
+      <vl-feature>
+        <vl-geom-point :coordinates="[38.726634,9.003391]"></vl-geom-point>
+      </vl-feature>
     </vl-map>
   </v-content>
 </template>
 
 <script>
-  export default {
+import axios from 'axios'
+
+export default {
     data () {
       return { 
-        zoom: 2,
-        center: [0, 0],
+        zoom: 17,
+        center: [38.7241, 9.0048],
         rotation: 0,
       }
     },
+  };
+
+  function point(){
+    axios.
+        get("/tower")
+        .then( (response) => console.log(response.data[0].shirota))
+        
+
   }
+  point();
 </script>
 
 <style>
