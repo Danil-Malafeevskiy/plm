960e1eb13228805ff53d578e838100f43062f95c
Maksim Khirug <maxkhirug@gmail.com>
2022-06-30 20:11:33 +0300

    Добавлены новые данные

--- plm/frontend/src/components/Map/MapArea.vue
+++ plm/frontend/src/components/Map/MapArea.vue
@@ -7,23 +7,49 @@
         <vl-source-osm></vl-source-osm>
       </vl-layer-tile>
 
-       <vl-feature>
+      <vl-feature>
         <vl-geom-multi-point :coordinates="cord"></vl-geom-multi-point>
       </vl-feature>
+
+      <vl-layer-vector ref="featuresLayer">
+        <vl-source-vector :features="features"></vl-source-vector>
+      </vl-layer-vector>
+
+      <vl-interaction-select :features.sync="selectedFeatures">
+        <template slot-scope="select">
+          <vl-overlay v-for="feature in select.features" :key="feature.id" :id="feature.id"
+                        :position="feature.geometry.coordinates" :auto-pan="true">
+            <div id="card">
+              Точка № {{ feature.properties.id }}<br>
+              Номер опоры: {{ feature.properties.number_support }} <br>
+              ВЛ: {{ feature.properties.VL }} <br>
+              Тип опоры: {{ feature.properties.type_support }} <br>
+              Шифр опоры: {{ feature.properties.code_support }} <br>
+              Материал: {{ feature.properties.material }} <br>
+              Угол: {{ feature.properties.corner }} <br>
+              Высота: {{ feature.properties.height }}
+            </div>
+          </vl-overlay>
+        </template>
+      </vl-interaction-select>
+
     </vl-map>
   </v-content>
 </template>
 
 <script>
+import features from '@/components/Map/coordinates.js';
+  
 import axios from 'axios'
-
 export default {
     data () {
       return { 
-        zoom: 2,
-        center: [38.7241, 9.0048],
+        zoom: 13,
+        center: [56.105601504697127, 54.937854572222477],
         rotation: 0,
         cord: [],
+        features: features,
+
       }
     },
     methods: {
@@ -44,5 +70,14 @@ export default {
 <style>
   #content{
     margin-top: 40em;
+  }
+
+  #card{
+    background: white; 
+    border: 1px solid grey; 
+    color: black;
+    padding: 0.5em;
+    border-radius: 8%;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   }
 </style>
