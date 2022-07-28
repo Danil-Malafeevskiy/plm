eb07044c12e8df7e9c871b16f8e50397314a2420
danil87 <81032901+danil87@users.noreply.github.com>
2022-07-28 19:27:09 +0500

    Начата работа над карточкой пользователя

--- plm/frontend/src/components/HelpfulFunctions/Card.vue
+++ plm/frontend/src/components/HelpfulFunctions/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-card v-show="cardVisable_.data === true">
+    <v-card class="card_of_object" v-show="cardVisable_.data === true">
         <div class="card__window" v-if="addCardOn_.data">
             <p style="display: none;">{{ feature }}</p>
             <v-file-input class="pa-0 ma-0" height="37.53%" color="#EE5E5E" :prepend-icon="icon" hide-input>
@@ -126,10 +126,10 @@ export default {
                 this.cardVisable_ = this.cardVisable;
                 if (this.cardVisable_.data) {
                     if (document.querySelector('.v-navigation-drawer').clientWidth * 100 / 1920 < 18) {
-                        document.querySelector('.v-card').style.cssText = 'width: 38.05% !important; left: 60.28% !important;';
+                        document.querySelector('.card_of_object').style.cssText = 'width: 38.05% !important; left: 60.28% !important;';
                     }
                     else {
-                        document.querySelector('.v-card').style.cssText = 'width: 31.91% !important; left: 67.22% !important;';
+                        document.querySelector('.card_of_object').style.cssText = 'width: 31.91% !important; left: 67.22% !important;';
                     }
                 }
             }, deep: true
@@ -157,3 +157,81 @@ export default {
     },
 }
 </script>
+
+<style>
+.show__card {
+  margin-right: 8px;
+  border-radius: 8px !important;
+}
+
+.v-window__container {
+  min-height: 100%;
+}
+
+.card_of_object {
+  z-index: 1 !important;
+  min-height: 92.08% !important;
+  position: absolute !important;
+  left: 60.28% !important;
+  top: 4.19% !important;
+  border-radius: 12px !important;
+}
+
+.card__info {
+  align-items: center;
+}
+
+.card__footer {
+  align-items: flex-end;
+  display: flex;
+  bottom: 0px;
+  justify-content: flex-end;
+  border-top: 1px solid #E0E0E0;
+  border-radius: 0 0 12px 12px !important;
+  background-color: white;
+}
+
+.v-icon--link .v-icon__svg {
+  min-width: 133.33px !important;
+  min-height: 133.33px !important;
+  fill: #FFFFFF !important;
+}
+
+.card__window {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  min-width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 12px;
+}
+
+.card__img {
+  align-items: flex-start;
+}
+
+.v-input__icon,
+.v-icon--link {
+  min-width: 100% !important;
+  height: 100% !important;
+  min-height: 100% !important;
+  justify-content: center !important;
+  align-items: center !important;
+}
+
+.v-icon--link::after {
+  background-color: rgba(255, 255, 255, 0) !important;
+}
+
+.v-file-input {
+  min-height: 37.53%;
+  background-color: #EE5E5E;
+  border-radius: 12px 12px 0 0;
+}
+
+.row {
+  padding: 24px 24px 12px 24px !important;
+}
+</style>
